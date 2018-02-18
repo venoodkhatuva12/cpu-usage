8c1dc059be55e7ef30b8bc8edd4fdc7f86da211c
Vinood Katuwa <vinod.katuwa12@gmail.com>
2018-02-19 01:31:18 +0530

    newwnm
    m

---
+++ cpu.sh
@@ -0,0 +1,12 @@
+limit=2
+email_destination=mfaexceptionnotification@ea.com
+email_body="WARNING the CPU usage on server `hostname` exceeded the limit"
+email_subject="WARNING cpu usage"
+usage=`grep 'cpu' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}' | bc`
+test(){
+if echo $usage $limit | awk '{exit $1>$2?0:1}'
+then
+  echo -e $email_body | mail -s "$email_subject" $email_destination
+fi
+}
+test
