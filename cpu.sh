b52c48dfe8ea49613351c0d1a6776f1ac3a41a1a
Vinood Katuwa <vinod.katuwa12@gmail.com>
2018-03-21 13:33:58 +0530

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,5 +1,5 @@
 limit=2
-email_destination=mfaexceptionnotification@ea.com
+email_destination=user@domain.com
 email_body="WARNING the CPU usage on server `hostname` exceeded the limit"
 email_subject="WARNING cpu usage"
 usage=`grep 'cpu' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}' | bc`
